1eeeca00457bcc8e6abe4680f8a8d69b73ca1679
Hidde Folkertsma <h.folkertsma.1@student.rug.nl>
2022-01-14 11:37:35 +0100

    Choose a random RIR for each row

--- create_embedding_column.sh
+++
@@ -1,7 +0,0 @@
-storage_dir="/media/hidde/Storage/datasets"
-librispeech_dir=$storage_dir/LibriSpeech
-metadata_dir=metadata/Libri2Mix
-
-python scripts/create_embedding_column.py \
-    --librispeech_dir $librispeech_dir \
-    --metadata_dir $metadata_dir
--- scripts/create_additional_columns.py
+++ scripts/create_additional_columns.py
@@ -4,6 +4,7 @@ import argparse
 import pandas as pd
 import random
 import tqdm.contrib.concurrent
+import glob
 
 
 parser = argparse.ArgumentParser()
@@ -11,6 +12,8 @@ parser.add_argument('--librispeech_dir', type=str, required=True,
                     help='Path to librispeech root directory')
 parser.add_argument('--metadata_dir', type=str, required=True,
                     help='Path to the LibriMix metadata directory')
+parser.add_argument('--rir_dir', type=str, required=True,
+                    help='Path to the RIR directory')
 
 
 def main(args):
@@ -18,23 +21,21 @@ def main(args):
     librispeech_dir = args.librispeech_dir
     # Get metadata directory
     metadata_dir = args.metadata_dir
-    choose_embeddings(librispeech_dir, metadata_dir)
-
-
-def choose_embeddings(librispeech_dir, metadata_dir):
-    """ Create embedding utterances for the LibriMix dataset """
+    # Get room impulse response directory
+    rir_dir = args.rir_dir
     # Get metadata files
     md_filename_list = [
         file for file in os.listdir(metadata_dir) if 'info' not in file
     ]
-    # Create all parts of librimix
+    # Create additional columns for embedding and RIR paths
     for md_filename in md_filename_list:
         print(md_filename)
         csv_path = os.path.join(metadata_dir, md_filename)
-        process_metadata_file(csv_path, librispeech_dir)
+        choose_embeddings(csv_path, librispeech_dir)
+        choose_rirs(csv_path, rir_dir)
 
 
-def process_metadata_file(csv_path, librispeech_dir):
+def choose_embeddings(csv_path, librispeech_dir):
     """ Process a metadata generation file to choose embedding utterances """
     md_file = pd.read_csv(csv_path, engine='python')
     all_primaries = md_file['source_1_path']
@@ -61,8 +62,21 @@ def process_row(librispeech_dir, all_primaries, row):
         and utt.endswith('.flac')    # ignore the transcript .txt files
         and utt not in all_primaries # don't use any row's primary, incl. current row
     ]
-    
+
     return random.choice(utterances)
+
+
+def choose_rirs(csv_path, rir_dir):
+    md_file = pd.read_csv(csv_path, engine='python')
+    # Read all RIR paths relative to rir_dir
+    rir_paths = [
+        os.path.relpath(y, rir_dir) for x in os.walk(rir_dir)
+        for y in glob.glob(os.path.join(x[0], '*.wav'))
+    ]
+    # Choose a random unique RIR for each row
+    rir_paths = random.sample(rir_paths, md_file.shape[0])
+    md_file['rir_path'] = rir_paths
+    md_file.to_csv(csv_path, index=False)
 
 
 if __name__ == "__main__":
