69a7331fa370f8f9beec4ef259805b2dee3aeb8d
Hidde Folkertsma <h.folkertsma.1@student.rug.nl>
2022-01-25 00:14:07 +0100

    Fix bug where we would always augment

--- scripts/augment_train_noise.py
+++ scripts/augment_train_noise.py
@@ -23,24 +23,22 @@ def main(args):
         os.mkdir(aug_dir)
         print(f'Augmenting {subdir} files')
         # Transform audio speed
-        augment_noise(sound_paths, 0.8)
-        augment_noise(sound_paths, 1.2)
+        # augment_noise(sound_paths, 0.8)
+        # augment_noise(sound_paths, 1.2)
+        return
 
     aug_paths = glob.glob(os.path.join(aug_dir, '**/*.wav'), recursive=True)
-    if len(aug_paths) != 30:
+    if len(aug_paths) != 40000:
         print("It appears that augmented files have not been generated properly\n"
               "Resuming augmentation.")
         to_be_removed_08 = [x.replace('sp08','') for x in aug_paths if 'sp08' in x]
         to_be_removed_12 = [x.replace('sp12','') for x in aug_paths if 'sp12' in x ]
         sound_paths_08 = list(set(sound_paths) - set(to_be_removed_08))
         sound_paths_12 = list(set(sound_paths) - set(to_be_removed_12))
-        augment_noise(sound_paths_08, 0.8)
-        augment_noise(sound_paths_12, 1.2)
+        # augment_noise(sound_paths_08, 0.8)
+        # augment_noise(sound_paths_12, 1.2)
     else:
-        print(f'Augmenting {subdir} files')
-        # Transform audio speed
-        augment_noise(sound_paths, 0.8)
-        augment_noise(sound_paths, 1.2)
+        print(f'Augmented files already generated in {aug_dir}')
 
 
 def augment_noise(sound_paths, speed):
