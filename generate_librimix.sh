437ef341d23799e167601b0b7042458fd68138bb
hiddefolkertsma <h.folkertsma.1@student.rug.nl>
2022-02-02 20:07:37 +0000

    Add Libri1Mix to generation script

--- generate_librimix.sh
+++ generate_librimix.sh
@@ -7,7 +7,7 @@ wham_dir=$storage_dir/wham_noise/chunked_20s
 librimix_outdir=$storage_dir
 python_path=python3
 
-for n_src in 2 3; do
+for n_src in 1 2 3; do
     metadata_dir=metadata/Libri$n_src"Mix"
     $python_path scripts/create_librimix_from_metadata.py \
         --librispeech_dir $librispeech_dir \
