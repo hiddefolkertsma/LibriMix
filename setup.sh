f97c1ab17019b5b48ae12535dbada6a6a57b0593
hiddefolkertsma <h.folkertsma.1@student.rug.nl>
2022-02-02 20:06:33 +0000

    Refactor setup script

--- setup.sh
+++ setup.sh
@@ -5,50 +5,19 @@ storage_dir=$1
 librispeech_dir=$storage_dir/LibriSpeech
 wham_dir=$storage_dir/wham_noise
 
-function LibriSpeech_dev_clean() {
-	if ! test -e $librispeech_dir/dev-clean; then
-		echo "Download LibriSpeech/dev-clean into $storage_dir"
+function download_librispeech() {
+	if ! test -d $librispeech_dir/$1; then
+		echo "Downloading LibriSpeech/$1 into $storage_dir"
 		# If downloading stalls for more than 20s, relaunch from previous state.
-		wget -c --tries=0 --read-timeout=20 http://www.openslr.org/resources/12/dev-clean.tar.gz -P $storage_dir
-		tar -xzf $storage_dir/dev-clean.tar.gz -C $storage_dir
-		rm -rf $storage_dir/dev-clean.tar.gz
+		wget -c --tries=0 --read-timeout=20 http://www.openslr.org/resources/12/$1.tar.gz -P $storage_dir
+		tar -xzf $storage_dir/$1.tar.gz -C $storage_dir
+		rm -rf $storage_dir/$1.tar.gz
 	fi
 }
 
-function LibriSpeech_test_clean() {
-	if ! test -e $librispeech_dir/test-clean; then
-		echo "Download LibriSpeech/test-clean into $storage_dir"
-		# If downloading stalls for more than 20s, relaunch from previous state.
-		wget -c --tries=0 --read-timeout=20 http://www.openslr.org/resources/12/test-clean.tar.gz -P $storage_dir
-		tar -xzf $storage_dir/test-clean.tar.gz -C $storage_dir
-		rm -rf $storage_dir/test-clean.tar.gz
-	fi
-}
-
-function LibriSpeech_clean100() {
-	if ! test -e $librispeech_dir/train-clean-100; then
-		echo "Download LibriSpeech/train-clean-100 into $storage_dir"
-		# If downloading stalls for more than 20s, relaunch from previous state.
-		wget -c --tries=0 --read-timeout=20 http://www.openslr.org/resources/12/train-clean-100.tar.gz -P $storage_dir
-		tar -xzf $storage_dir/train-clean-100.tar.gz -C $storage_dir
-		rm -rf $storage_dir/train-clean-100.tar.gz
-	fi
-}
-
-function LibriSpeech_clean360() {
-	if ! test -e $librispeech_dir/train-clean-360; then
-		echo "Download LibriSpeech/train-clean-360 into $storage_dir"
-		# If downloading stalls for more than 20s, relaunch from previous state.
-		wget -c --tries=0 --read-timeout=20 http://www.openslr.org/resources/12/train-clean-360.tar.gz -P $storage_dir
-		tar -xzf $storage_dir/train-clean-360.tar.gz -C $storage_dir
-		rm -rf $storage_dir/train-clean-360.tar.gz
-	fi
-}
-
-function wham() {
-	if ! test -e $wham_dir; then
-		echo "Download wham_noise into $storage_dir"
-
+function download_wham() {
+	if ! test -d $wham_dir; then
+		echo "Downloading wham_noise into $storage_dir"
 		# If downloading stalls for more than 20s, relaunch from previous state.
 		wget -c --tries=0 --read-timeout=20 https://storage.googleapis.com/whisper-public/wham_noise.zip -P $storage_dir
 		unzip -qn $storage_dir/wham_noise.zip -d $storage_dir
@@ -56,23 +25,24 @@ function wham() {
 	fi
 }
 
-LibriSpeech_dev_clean &
-LibriSpeech_test_clean &
-LibriSpeech_clean100 &
-LibriSpeech_clean360 &
-wham &
+download_librispeech dev-clean &
+download_librispeech test-clean &
+download_librispeech train-clean-100 &
+download_librispeech train-clean-360 &
+download_librispeech train-other-500 &
+download_wham &
 
 wait
 
 # Path to python
 python_path=python3
 
+# Augment WHAM noise if necessary
 $python_path scripts/augment_train_noise.py --wham_dir $wham_dir
-wham_dir_chunked=$wham_dir/chunked_20s
 
 # Chunk WHAM into 20s segments
-if [[ ! -d "$wham_dir_chunked" ]]
-then
+wham_dir_chunked=$wham_dir/chunked_20s
+if ! test -d "$wham_dir_chunked"; then
     echo "Chunked WHAM not found. Creating chunked WHAM..."
 	$python_path scripts/chunk_speakers.py $wham_dir $wham_dir_chunked
 	$python_path scripts/create_wham_metadata.py --wham_dir $wham_dir_chunked -z 20
@@ -83,24 +53,25 @@ fi
 # Chunk LibriSpeech to 6s segments for computing embeddings
 librispeech_chunked=$librispeech_dir/chunked
 function chunk_librispeech() {
-        if ! test -e $librispeech_chunked/$1; then
-                echo "Chunking $1 into $librispeech_chunked/$1"
-                $python_path scripts/chunk_speakers.py $librispeech_dir/$1 $librispeech_chunked/$1
-                echo "Done chunking $1"
-        else
-                echo "Chunked $1 found in $librispeech_chunked/$1"
-        fi
+	if ! test -d $librispeech_chunked/$1; then
+		echo "Chunking $1 into $librispeech_chunked/$1"
+		$python_path scripts/chunk_speakers.py $librispeech_dir/$1 $librispeech_chunked/$1 -z 6
+		echo "Done chunking $1"
+	else
+		echo "Chunked $1 found in $librispeech_chunked/$1"
+	fi
 }
 
 chunk_librispeech dev-clean &
 chunk_librispeech test-clean &
 chunk_librispeech train-clean-100 &
 chunk_librispeech train-clean-360 &
+chunk_librispeech train-other-500 &
 
 wait
 
 # Create metadata
-for n_src in 2 3; do
+for n_src in 1 2 3; do
 	echo "Generating metadata for Libri$n_src"Mix
   	$python_path scripts/create_librimix_metadata.py \
     	--librispeech_dir $librispeech_dir \
